4441aa234d1e1023f7be91f920cd5d44597a074d
wmach30 <wmach30@stuy.edu>
2022-11-22 13:59:22 -0500

    add

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,9 +1,17 @@
 '''
-Shinji, Wilson
+Mew: Shinji Kusakabe, Wilson Mach
 SoftDev
-K<20> -- A RESTful Journey Skyward
-<2022>-<11>-<21>
+K20 -- A RESTful Journey Skyward
+2022-11-22
 time spent: 0.8 hours
+
+DISCO:
+1. You can use the requests library to make HTTP requests
+2. APIs are a good source of data
+3. JSON can be used like a dictionary  
+
+QCC: 
+1. How can we make our own APIs?
 '''
 
 from flask import Flask, render_template
