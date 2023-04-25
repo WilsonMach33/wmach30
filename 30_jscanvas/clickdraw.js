c45d7eaba92d03fa4a29982a698d5502b70472d0
unknown <wmach30@stuy.edu>
2023-04-24 20:00:47 -0400

    editing k30

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -1,3 +1,9 @@
+// Team Wandering Majestic Merciful Mice:: Maya M., Wilson
+// SoftDev pd2
+// K30 -- canvas based JS drawing
+// 2023-04-24m
+// HTML file for JavaScript canvas work
+
 // retrieve node in DOM via ID
 var c = document.getElementById("slate");
 
@@ -21,21 +27,47 @@ var toggleMode = (e) => {
 }
 
 //var drawRect = function(e) 
-var drawCircle = (e) => {
-    var mouseX = e.clientX; 
-    var mouseY = e.clientY;
-    console.log("mouseclick at ", mouseX, mouseY);
+var drawRect = (e) => {
+    // don't use clientX or clientY -> does not adjust coordinates based on device
+    var mouseX = e.offsetX; 
+    var mouseY = e.offsetY;
+    // console.log("mouseclick at ", mouseX, mouseY);
 
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(mouseX, mouseY, 100, 200);
+}
+
+//var drawCircle = function(e) 
+var drawCircle = (e) => {
+    // don't use clientX or clientY -> does not adjust coordinates based on device
+    var mouseX = e.offsetX; 
+    var mouseY = e.offsetY;
+    // console.log("mouseclick at ", mouseX, mouseY);
+
+    ctx.fillStyle = "#FF0000";
     ctx.beginPath();
-    ctx.arc(mouseX, mouseY, 10, 0, 360);
+    ctx.arc(mouseX, mouseY, 75, 0, 2*Math.PI);
     ctx.fill();
 }
 
+//var draw = function(e) 
+var draw = (e) => {
+    if(mode === "Rectangle"){
+        drawRect(e);
+    } else{
+        drawCircle(e);
+    }
+}
 
 
+//var wipeCanvas = function()
+var wipeCanvas = () => {
+    ctx.clearRect(0, 0, c.width, c.height);
+}
 
-c.addEventListener("click", drawCircle);
+c.addEventListener("click", draw);
 var bToggler = document.getElementById("buttonToggle");
 bToggler.addEventListener('click', toggleMode);
 
-
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener('click', wipeCanvas);
