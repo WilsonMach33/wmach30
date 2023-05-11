098d8c6e2285cc5d9a68a5a396ee55352ebd8f70
wmach30 <wmach30@stuy.edu>
2023-05-11 09:25:14 -0400

    ye

---
+++ D3_exploration/D3_test.js
@@ -0,0 +1,38 @@
+var svg = d3.select("svg"),
+            margin = 200,
+            width = svg.attr("width") - margin,
+            height = svg.attr("height") - margin
+
+
+var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+            yScale = d3.scaleLinear().range([height, 0]);
+
+var g = svg.append("g").attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+    d3.csv("https://Documents/wmach/D3_exploration/spotify_taylorswift.csv", function(error, data) {
+        if (error) {
+            throw error;
+        }
+
+        xScale.domain(data.map(function(d) { return d.name; }));
+        yScale.domain([0, d3.max(data, function(d) { return d.population; })]);
+
+        g.append("g")
+         .attr("transform", "translate(0," + height + ")")
+         .call(d3.axisBottom(xScale));
+
+        g.append("g")
+         .call(d3.axisLeft(yScale).tickFormat(function(d){
+             return "$" + d;
+         }).ticks(10));
+
+
+        g.selectAll(".bar")
+         .data(data)
+         .enter().append("rect")
+         .attr("class", "bar")
+         .attr("x", function(d) { return xScale(d.name); })
+         .attr("y", function(d) { return yScale(d.population); })
+         .attr("width", xScale.bandwidth())
+         .attr("height", function(d) { return (height - yScaled.population); });
+        });
